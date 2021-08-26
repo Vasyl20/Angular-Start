032b2130c2763c26ebf6cddba4609344e56aa3e9
MKA <mka@rivne.itstep.org>
2021-08-26 19:25:21 +0300

    test-0.1

--- Angular-Start/src/app/app.module.ts
+++ Angular-Start/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { Todo_listComponent } from './Components/todo_list/todo_list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    Todo_listComponent
   ],
   imports: [
     BrowserModule,
